a5b01a720881d98f7a560de91eac17e23e74e486
Saumya Shah <68504323+LordSaumya@users.noreply.github.com>
2024-06-27 00:57:42 +0800

    Remove unnecessary functions and add appropriate panics

--- rusty_kan/src/data_structures/vector.rs
+++ rusty_kan/src/data_structures/vector.rs
@@ -1,5 +1,4 @@
-use std::{ops::{Add, Div, Index, IndexMut, Mul, Sub}, iter::{Iterator, Map}};
-use serde::{Serialize, Deserialize};
+use std::{ops::{Add, Div, Index, IndexMut, Mul, Sub}, iter::Iterator};
 use crate::data_structures::matrix::Matrix;
 use rand::Rng;
 
@@ -7,7 +6,7 @@ use rand::Rng;
 /// It is represented as a list of elements.
 /// The vector struct implements basic operations such as addition, subtraction, multiplication, and division.
 /// It also provides methods to calculate the dot product, cross product, element-wise product, and convert to a matrix.
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Vector {
     pub elements: Vec<f64>,
 }
@@ -16,6 +15,9 @@ impl Add<Vector> for Vector {
     type Output = Vector;
 
     fn add(self, other: Vector) -> Vector {
+        if self.len() != other.len() {
+            panic!("Vectors must have the same length for addition.");
+        }
         Vector { elements: self.elements.iter().zip(other.elements.iter()).map(|(a, b)| a + b).collect() }
     }
 }
@@ -24,6 +26,9 @@ impl Add<&Vector> for &Vector {
     type Output = Vector;
 
     fn add(self, other: &Vector) -> Vector {
+        if self.len() != other.len() {
+            panic!("Vectors must have the same length for addition.");
+        }
         Vector { elements: self.elements.iter().zip(other.elements.iter()).map(|(a, b)| a + b).collect() }
     }
 }
@@ -32,6 +37,9 @@ impl Sub<Vector> for Vector {
     type Output = Vector;
 
     fn sub(self, other: Vector) -> Vector {
+        if self.len() != other.len() {
+            panic!("Vectors must have the same length for subtraction.");
+        }
         Vector { elements: self.elements.iter().zip(other.elements.iter()).map(|(a, b)| a - b).collect() }
     }
 }
@@ -40,6 +48,9 @@ impl Sub<&Vector> for &Vector {
     type Output = Vector;
 
     fn sub(self, other: &Vector) -> Vector {
+        if self.len() != other.len() {
+            panic!("Vectors must have the same length for subtraction.");
+        }
         Vector { elements: self.elements.iter().zip(other.elements.iter()).map(|(a, b)| a - b).collect() }
     }
 }
@@ -64,6 +75,9 @@ impl Div<f64> for Vector {
     type Output = Vector;
 
     fn div(self, scalar: f64) -> Vector {
+        if scalar == 0.0 {
+            panic!("Cannot divide by zero.");
+        }
         Vector { elements: self.elements.iter().map(|x| x / scalar).collect() }
     }
 }
@@ -72,6 +86,9 @@ impl Div<f64> for &Vector {
     type Output = Vector;
 
     fn div(self, scalar: f64) -> Vector {
+        if scalar == 0.0 {
+            panic!("Cannot divide by zero.");
+        }
         Vector { elements: self.elements.iter().map(|x| x / scalar).collect() }
     }
 }
@@ -141,20 +158,17 @@ impl Vector {
 
     /// Return the dot product of two vectors.
     pub fn dot(&self, other: &Vector) -> f64 {
-        self.element_wise(other).elements.iter().sum()
-    }
-
-    /// Return the cross product of two vectors.
-    pub fn cross(&self, other: &Vector) -> Vector {
-        let mut result: Vec<f64> = Vec::with_capacity(self.len());
-        for i in 0..self.len() {
-            result.push(self.elements[(i + 1) % self.len()] * other.elements[(i + 2) % self.len()] - self.elements[(i + 2) % self.len()] * other.elements[(i + 1) % self.len()]);
+        if self.len() != other.len() {
+            panic!("Vectors must have the same length for the dot product.");
         }
-        Vector::new(result)
+        self.element_wise(other).elements.iter().sum()
     }
 
     /// Return the element-wise product of two vectors.
     pub fn element_wise(&self, other: &Vector) -> Vector {
+        if self.len() != other.len() {
+            panic!("Vectors must have the same length for element-wise multiplication.");
+        }
         Vector { elements: self.elements.iter().zip(other.elements.iter()).map(|(a, b)| a * b).collect() }
     }
 
